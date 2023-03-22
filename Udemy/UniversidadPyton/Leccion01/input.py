c4988c59f49d672188a3024889d57ea1ca69c085
Daniel Serrano Serrano <dannyserranoserrano@gmail.com>
2023-03-22 19:13:23 +0100

    Carga desde Pycharm

--- Udemy/UniversidadPyton/Leccion01/input.py
+++ Udemy/UniversidadPyton/Leccion01/input.py
@@ -1,6 +1,9 @@
 #Funcion Input para procesar la entrada del usuario
-numero1= input("Escribe el primer numero: ")
-numero2= input("Escribe el segundo numero: ")
+numero1= int(input("Escribe el primer numero: "))
+numero2= int(input("Escribe el segundo numero: "))
 resultado = numero1 + numero2
-print("Valor Proporcionado: " resultado)
+print("Valor Proporcionado: ", resultado)
 print("Fin del programa")
+
+valor = int(input("Como estuvo tu dia?(1 a 10): "))
+print("Mi dia estuvo de: " + valor)
