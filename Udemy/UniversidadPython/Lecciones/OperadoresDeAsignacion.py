28845f8dad65f64d595a83b19fc9da8d2c0683a2
Daniel Serrano Serrano <dannyserranoserrano@gmail.com>
2023-03-22 22:57:11 +0100

    Carga desde Pycharm

---
+++ Udemy/UniversidadPython/Lecciones/OperadoresDeAsignacion.py
@@ -0,0 +1,12 @@
+miVariable = 10
+print(miVariable)
+
+#Incremento con reasignacion
+miVariable = miVariable - 1
+print(miVariable)
+
+miVariable *= 10
+print(miVariable)
+
+miVariable /= 6
+print(miVariable)
