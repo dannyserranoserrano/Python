0e06c61163c2dc128483589e275ca591696d5841
Daniel Serrano Serrano <dannyserranoserrano@gmail.com>
2023-03-23 09:46:01 +0100

    Carga desde Pycharm

--- Udemy/UniversidadPython/Lecciones/OperadoresLogicos.py
+++ Udemy/UniversidadPython/Lecciones/OperadoresLogicos.py
@@ -10,3 +10,17 @@ resultado  = not a
 print(resultado)
 resultado  = not b
 print(resultado)
+
+#Ejercicio Valor dentro del rango
+valor = int(input('Introduce un digito: '))
+valorMin = 0
+valorMax = 5
+
+dentroRango = (valor >= valorMin) and (valor <= valorMax)
+print(f'El valor {valor} está dentro de rango ') if dentroRango else print(f'El valor {valor} está fuera de rango ')
+
+#Ejercicio con operador OR
+vacaciones = True
+diaDescanso = False
+
+print('Puede asistir al partido') if vacaciones or diaDescanso else print('No puede asistir al partido')
