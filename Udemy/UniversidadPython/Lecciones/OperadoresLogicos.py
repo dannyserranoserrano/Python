4c531c34d2e5a612f7c51f8ea147278dbbe08e53
Daniel Serrano Serrano <dannyserranoserrano@gmail.com>
2023-03-23 22:45:07 +0100

    Carga desde Pycharm

--- Udemy/UniversidadPython/Lecciones/OperadoresLogicos.py
+++ Udemy/UniversidadPython/Lecciones/OperadoresLogicos.py
@@ -21,6 +21,30 @@ print(f'El valor {valor} está dentro de rango ') if dentroRango else print(f'E
 
 #Ejercicio con operador OR
 vacaciones = True
-diaDescanso = False
+diaDescanso = True
 
-print('Puede asistir al partido') if vacaciones or diaDescanso else print('No puede asistir al partido')
+print('No puede asistir al partido') if not (vacaciones or diaDescanso) else print('Puede asistir al partido')
+
+#Ejercicio AND OR
+edad = int(input("Introduce tu edad: "))
+veintes = edad >= 20 and edad < 30
+treintas = edad >= 30  and edad < 40
+print('Dentro de los rangos (20\'s o 30\'s)') if veintes or treintas else print("No está dentro de los 20's ni 30's")
+
+#Otras formas
+if(20 <= edad < 30) or (30 <= edad <40):
+    print('Dentro de rango (20\'s) o (30\'s)')
+else:
+    print("No está dentro de los 20's ni 30's")
+
+#Ejercicio Cual es mayor
+valor1 = int(input("Introduce el primer valor: "))
+valor2 = int(input("Introduce el segundo valor: "))
+
+if(valor1 > valor2):
+    print(f'{valor1} es superior a {valor2}: ')
+elif(valor1 < valor2):
+    print(f'{valor2} es superior a {valor1}: 4')
+else:
+    print(f'{valor1} es igual a {valor2}')
+
