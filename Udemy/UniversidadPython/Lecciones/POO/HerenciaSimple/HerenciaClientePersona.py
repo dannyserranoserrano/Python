478c68b6f56f7f076d18adf1ef4bb5e92729512f
Daniel Serrano Serrano <dannyserranoserrano@gmail.com>
2023-04-25 23:38:43 +0200

    Carga desde Pycharm

--- Udemy/UniversidadPython/Lecciones/POO/HerenciaSimple/HerenciaClientePersona.py
+++
@@ -1,7 +0,0 @@
-from Python.Udemy.UniversidadPython.Lecciones.POO.HerenciaSimple.HerenciaPersona import *
-
-persona1 = Persona('Juan', 28)
-print(persona1)
-
-empleado1 = Empleado("Carla", 30, 5000)
-print(empleado1)
