59af22e76cf64074c012f7d006397c0977ea6741
hiberusmint <dannyserranoserrano@gmail.com>
2023-05-10 00:07:50 +0200

    Carga desde Pycharm

--- Udemy/UniversidadPython/POO/Leccion07/Producto.py
+++ Udemy/UniversidadPython/POO/Leccion07/Producto.py
@@ -1,12 +1,15 @@
 class Producto:
     contador_productos = 0
 
-    def  __init__(self, nombre, precio):
+    def __init__(self, nombre, precio):
         Producto.contador_productos += 1
         self._id_producto = Producto.contador_productos
         self._nombre = nombre
         self._precio = precio
 
+    @property
+    def precio(self):
+        return self._precio
 
     def __str__(self):
         return f'Id Producto: {self._id_producto}, Nombre: {self._nombre}, Precio: {self._precio}'
